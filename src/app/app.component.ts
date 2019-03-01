84af3bdf30942c3e3606ba49370eef64e617f47d
Talent Malaba <malabatalent@gmail.com>
2019-03-01 14:56:38 +0800

    removed navbar and used Angular material tabs instead

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  navLinks: any[];
+  activeLinkIndex = -1;
+  constructor(private router: Router) {
+    this.navLinks = [
+      {
+        label: 'Search Hashtag',
+        link: './hashtags',
+        index: 0
+      }, {
+        label: 'Search User',
+        link: './users',
+        index: 1
+      }
+    ];
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(res => {
+      this.activeLinkIndex = this.navLinks.indexOf(this.navLinks.find(tab => tab.link === '.' + this.router.url));
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { HashtagsComponent } from './hashtags/hashtags.component';
 import { UsersComponent } from './users/users.component';
 
 import { RoutingModule } from './router.module';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule, MatToolbarModule } from '@angular/material';
 import { DataTableModule } from 'angular-6-datatable';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     HashtagsComponent,
     UsersComponent,
   ],
@@ -21,6 +21,9 @@ import { DataTableModule } from 'angular-6-datatable';
     BrowserModule,
     RoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatTabsModule,
     DataTableModule,
   ],
   providers: [],
